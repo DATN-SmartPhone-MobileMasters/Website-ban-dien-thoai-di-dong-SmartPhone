ea13574d132e9bd737996880758a1803de43db45
tuannguyen-1309 <112544396+tuannguyen-1309@users.noreply.github.com>
2025-01-03 21:29:06 +0700

    Delete backend/validations directory

--- backend/validations/auth.js
+++
@@ -1,8 +0,0 @@
-import Joi from "joi"; //import joi
-
-const authValidator = Joi.object({
-  email: Joi.string().required().email(),
-  password: Joi.string().required().min(6),
-});
-
-export { authValidator };
