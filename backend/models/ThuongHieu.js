fa704b7e52dc6067f3a921e9efabe691bfa67422
tuannguyen-1309 <tuandungyn2@gmail.com>
2025-01-14 16:19:13 +0700

    update schema thương hiệu

--- backend/models/ThuongHieu.js
+++ backend/models/ThuongHieu.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
-//schema sản phẩm
 
 const thuongHieuSchema = new mongoose.Schema({
   TenTH: { type: String, required: true },
   HinhAnh: { type: String },
   Mota: { type: String },
-  MaDM: { type: String, ref: "danhmuc" }, // tham chiếu đúng tên bảng
+  MaDM: { type: mongoose.Schema.Types.ObjectId, ref: "danhmuc" }, // Tham chiếu tới bảng "danhmuc"
   created_at: { type: Date, default: Date.now },
 });
 
 const thuonghieu = mongoose.model("thuonghieu", thuongHieuSchema);
 export default thuonghieu;
+
