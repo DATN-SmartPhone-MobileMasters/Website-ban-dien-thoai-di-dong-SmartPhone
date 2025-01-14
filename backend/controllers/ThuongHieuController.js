6dc187a32ac9376e636fc1d49bb1deb5a498b0dd
tuannguyen-1309 <tuandungyn2@gmail.com>
2025-01-14 16:19:45 +0700

    update các chức năng bảng thương hiệu với bảng danh mục

--- backend/controllers/ThuongHieuController.js
+++ backend/controllers/ThuongHieuController.js
@@ -1,11 +1,12 @@
-import ThuongHieu from "../models/ThuongHieu.js";
+import thuonghieu from "../models/ThuongHieu.js";
 
 //khởi tạo class
 class ThuongHieuController {
   //API functions
   async apiList(req, res) {
     try {
-      const thuongHieus = await ThuongHieu.find();
+      // Sử dụng populate để lấy thông tin từ danh mục
+      const thuongHieus = await thuonghieu.find().populate("MaDM", "TenDM");
       res.status(200).json({
         message: "Lấy dữ liệu thành công",
         data: thuongHieus,
@@ -18,26 +19,10 @@ class ThuongHieuController {
     }
   }
 
-  async apiDetail(req, res) {
-    try {
-      const id = req.params.id;
-      const thuongHieu = await ThuongHieu.findById(id);
-      res.status(200).json({
-        message: "Thành công",
-        data: thuongHieu,
-      });
-    } catch (error) {
-      res.status(500).json({
-        message: "Lỗi khi lấy chi tiết",
-        error: error.message,
-      });
-    }
-  }
-
   async apiDelete(req, res) {
     try {
       const id = req.params.id;
-      const deletedThuongHieu = await ThuongHieu.findByIdAndDelete(id);
+      const deletedThuongHieu = await thuonghieu.findByIdAndDelete(id);
       res.status(200).json({
         message: "Xóa thành công",
         data: deletedThuongHieu,
@@ -50,10 +35,36 @@ class ThuongHieuController {
     }
   }
 
+  async apiDetail(req, res) {
+  try {
+    const id = req.params.id;
+
+    // Sử dụng populate để lấy tên danh mục
+    const thuongHieu = await thuonghieu.findById(id).populate("MaDM", "TenDM");
+
+    if (!thuongHieu) {
+      return res.status(404).json({
+        message: "Không tìm thấy thương hiệu",
+      });
+    }
+
+    res.status(200).json({
+      message: "Thành công",
+      data: thuongHieu,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Lỗi khi lấy chi tiết",
+      error: error.message,
+    });
+  }
+}
+
+
   async apiCreate(req, res) {
     try {
       const data = req.body;
-      const newThuongHieu = await ThuongHieu.create(data);
+      const newThuongHieu = await thuonghieu.create(data);
       res.status(200).json({
         message: "Thành công",
         data: newThuongHieu,
@@ -70,7 +81,7 @@ class ThuongHieuController {
     try {
       const id = req.params.id;
       const data = req.body;
-      const thuongHieu = await ThuongHieu.findByIdAndUpdate(id, data);
+      const thuongHieu = await thuonghieu.findByIdAndUpdate(id, data).populate("MaDM", "TenDM");
       res.status(200).json({
         message: "Thành công",
         data: thuongHieu,
