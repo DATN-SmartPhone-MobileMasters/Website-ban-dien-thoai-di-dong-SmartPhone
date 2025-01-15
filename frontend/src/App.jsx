04958182867d0eaf74d261e0db01d44879d5f9c2
tuannguyen-1309 <tuandungyn2@gmail.com>
2025-01-15 21:18:30 +0700

    remove

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,73 +2,22 @@ import { useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LayoutAdmin from "./pages/(admin)/LayoutAdmin";
 import DashBoard from "./pages/(admin)/DashBoard";
-<<<<<<< HEAD
-import CategoryList from "./pages/(admin)/categorys/CategoryList";
-import AddCategory from "./pages/(admin)/categorys/AddCategory";
-import UpdateCategory from "./pages/(admin)/categorys/UpdateCategory";
-
-import ProductsList from "./pages/(admin)/products/ProductsList";
-import ProductsEdit from "./pages/(admin)/products/ProductsEdit";
-import ProductsAdd from "./pages/(admin)/products/ProductsAdd";
-import ProductsDetail from "./pages/(admin)/products/ProductsDetail";
-import AddPromotion from "./pages/(admin)/Promotion/AddPromotion";
-import UpdatePromotion from "./pages/(admin)/Promotion/UpdatePromotion";
-import Promotion from "../../backend/models/Promotion";
 
 import BrandList from "./pages/(admin)/Brands/BrandList";
 import BrandAdd from "./pages/(admin)/Brands/BrandAdd";
 import BrandEdit from "./pages/(admin)/Brands/BrandEdit";
 import BrandDetail from "./pages/(admin)/Brands/BrandDetail";
-import AdminListComment from "./pages/(admin)/Comment/ListComments";
-import AdminDetailComment from "./pages/(admin)/Comment/DetailComment";
-function App() {
-  const configRouter = createBrowserRouter([
-=======
-import ListOrder from "./pages/(admin)/order/ListOrder";
-import UpdateOrderStatus from "./pages/(admin)/order/UpdateOrderStatus";
+
 function App() {
   const configRouter = createBrowserRouter([
     // Layout Admin
->>>>>>> dao-duy-khanh
     {
       element: <LayoutAdmin />,
       children: [
         // dashboard admin
-<<<<<<< HEAD
-
         {
           path: "/",
           element: <DashBoard />,
-        },
-
-        {
-          path: "/categorys",
-          element: <CategoryList />,
-        },
-        {
-          path: "/categorys/addcategory",
-          element: <AddCategory />,
-        },
-        {
-          path: "/categorys/updatecategory/:id",
-          element: <UpdateCategory />,
-        },
-        {
-          path: "/ProductsList",
-          element: <ProductsList />,
-        },
-        {
-          path: "/ProductsEdit/:id",
-          element: <ProductsEdit />,
-        },
-
-        {
-          path: "/ProductsAdd",
-          element: <ProductsAdd />,
-        },
-        {
-          path: "/ProductsDetail/:id",
-          element: <ProductsDetail />,
         },
 
         {
@@ -87,38 +36,10 @@ function App() {
           path: "/brands/detail/:id",
           element: <BrandDetail />,
         },
-        {
-          path: "/comments",
-          element: <AdminListComment />,
-        },
-        {
-          path: "/comments/:id",
-          element: <AdminDetailComment />,
-        },
-        {
-          path: "/promotions",
-          element: <Promotion />,
-        },
-        {
-          path: "/add/promotion",
-          element: <AddPromotion />,
-        },
-        {
-          path: "/edit/promotion/:id",
-          element: <UpdatePromotion />,
-=======
+
         {
           path: "/dashboard",
           element: <DashBoard />,
-        },
-        {
-          path: "/listorder",
-          element: <ListOrder />,
-        },
-        {
-          path: "/orders/updateStatus/:id",
-          element: <UpdateOrderStatus />,
->>>>>>> dao-duy-khanh
         },
       ],
     },
--- frontend/src/pages/(admin)/components/SideBar.jsx
+++ frontend/src/pages/(admin)/components/SideBar.jsx
@@ -54,17 +54,13 @@ const SideBar = () => {
             data-parent="#accordionSidebar"
           >
             <div className="bg-white py-2 collapse-inner rounded">
-<<<<<<< HEAD
               <Link className="collapse-item" to="/categorys">
                 Danh sách danh mục
               </Link>
-              <Link className="collapse-item" to="categorys/addcategory">
-=======
               <Link className="collapse-item" to="#">
                 Danh sách danh mục
               </Link>
               <Link className="collapse-item" to="#">
->>>>>>> dao-duy-khanh
                 Thêm mới danh mục
               </Link>
             </div>
@@ -90,7 +86,6 @@ const SideBar = () => {
             data-parent="#accordionSidebar"
           >
             <div className="bg-white py-2 collapse-inner rounded">
-<<<<<<< HEAD
               <Link className="collapse-item" to="/ProductsList">
                 Danh sách sản phẩm
               </Link>
@@ -101,13 +96,10 @@ const SideBar = () => {
               <Link className="collapse-item" to="#">
                 Danh sách sản phẩm
               </Link>
-              <Link className="collapse-item" to="categorys/addcategory">
-=======
               <Link className="collapse-item" to="/products">
                 Danh sách sản phẩm
               </Link>
               <Link className="collapse-item" to="#">
->>>>>>> dao-duy-khanh
                 Thêm mới sản phẩm
               </Link>
             </div>
@@ -133,10 +125,6 @@ const SideBar = () => {
             data-parent="#accordionSidebar"
           >
             <div className="bg-white py-2 collapse-inner rounded">
-<<<<<<< HEAD
-              <Link className="collapse-item" to="#">
-=======
->>>>>>> dao-duy-khanh
               <Link className="collapse-item" to="/brands">
                 Danh sách thương hiệu
               </Link>
@@ -148,11 +136,7 @@ const SideBar = () => {
         </li>
         {/* Other Items */}
         <li className="nav-item">
-<<<<<<< HEAD
-          <Link className="nav-link collapsed" to="#">
-=======
           <Link className="nav-link collapsed" to="/listorder">
->>>>>>> dao-duy-khanh
             <i className="fas fa-fw fa-wrench" />
             <span>Quản lý đơn hàng</span>
           </Link>
@@ -205,18 +189,7 @@ const SideBar = () => {
             data-parent="#accordionSidebar"
           >
             <div className="bg-white py-2 collapse-inner rounded">
-<<<<<<< HEAD
-
-=======
-
               <Link className="collapse-item" to="#">
-
-              <Link className="collapse-item" to="/comments">
-
-
-=======
-              <Link className="collapse-item" to="#">
->>>>>>> dao-duy-khanh
                 Danh sách bình luận
               </Link>
             </div>
@@ -231,13 +204,8 @@ const SideBar = () => {
             aria-expanded="true"
             aria-controls="collapseTaikhoan"
           >
-<<<<<<< HEAD
-
             <i className="fas fa-fw fa-user" />
-
-=======
             <i className="fas fa-fw fa-wrench" />
->>>>>>> dao-duy-khanh
             <span>Quản lý tài khoản</span>
           </Link>
           <div
@@ -247,12 +215,7 @@ const SideBar = () => {
             data-parent="#accordionSidebar"
           >
             <div className="bg-white py-2 collapse-inner rounded">
-<<<<<<< HEAD
-
-              <Link className="collapse-item" to="/accounts">
-=======
               <Link className="collapse-item" to="#">
->>>>>>> dao-duy-khanh
                 Danh sách tài khoản
               </Link>
             </div>
