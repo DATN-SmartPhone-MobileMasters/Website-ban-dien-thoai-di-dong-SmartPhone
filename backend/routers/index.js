34b6fd099ab0b9cfc676199416249a4c32163a33
khanhdao901 <khanhddph47074@fpt.edu.vn>
2025-01-15 17:46:33 +0700

    update_index_hoadon

--- backend/routers/index.js
+++ backend/routers/index.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import apiRouter from "./api.js";
-<<<<<<< HEAD
 import routePromotion from "./promotion.js";
 
 const router = Router();
@@ -9,16 +8,13 @@ router.use("/api", apiRouter); //router trả về api (dữ liệu dạng json)
 
 import cmtRouter from "./comment.js";
 
-const router = Router();
+
 
 router.use("/api", apiRouter); //router trả về api (dữ liệu dạng json)
 router.use("/cmt", cmtRouter); // router comment
 router.use("/api/v1/promotion", routePromotion);
-=======
 
-const router = Router(); 
 
 router.use('/api', apiRouter); //router trả về api (dữ liệu dạng json)
->>>>>>> dao-duy-khanh
 
 export default router;
