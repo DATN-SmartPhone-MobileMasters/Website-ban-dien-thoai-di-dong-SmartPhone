58ecfcea4a0eb2765b9b1e7a1b9bc614b1eeb64a
Anhnpdph47088 <162577181+Anhnpdph47088@users.noreply.github.com>
2025-01-15 21:40:52 +0700

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
-<<<<<<< HEAD
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-
-
-=======
->>>>>>> dao-duy-khanh
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
