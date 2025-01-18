d627fc40bf86a3dda9f3b24d0ef076a0ee121fd6
tuannguyen-1309 <tuandungyn2@gmail.com>
2025-01-18 11:18:51 +0700

    router thương hiệu fe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LayoutAdmin from "./pages/(admin)/LayoutAdmin";
 import DashBoard from "./pages/(admin)/DashBoard";
+import BrandList from "./pages/(admin)/Brands/BrandList";
+import BrandAdd from "./pages/(admin)/Brands/BrandAdd";
+import BrandEdit from "./pages/(admin)/Brands/BrandEdit";
+import BrandDetail from "./pages/(admin)/Brands/BrandDetail";
 
 function App() {
   const configRouter = createBrowserRouter([
@@ -13,6 +17,22 @@ function App() {
         {
           path: "/",
           element: <DashBoard />,
+        },
+        {
+          path: "/brands",
+          element: <BrandList />,
+        },
+        {
+          path: "/brands/add",
+          element: <BrandAdd />,
+        },
+        {
+          path: "/brands/edit/:id",
+          element: <BrandEdit />,
+        },
+        {
+          path: "/brands/detail/:id",
+          element: <BrandDetail />,
         },
       ],
     },
