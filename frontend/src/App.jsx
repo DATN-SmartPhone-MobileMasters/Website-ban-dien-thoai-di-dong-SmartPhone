e8c61e41bb22b4884f89260cabd0ded3ddbf9335
Nguyenngocthang1234 <thangbek123@gmail.com>
2025-01-23 00:45:22 +0700

    update CRUD danhmuc fontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,9 @@ import ProductsList from "./pages/(admin)/products/ProductsList";
 import ProductsEdit from "./pages/(admin)/products/ProductsEdit";
 import ProductsAdd from "./pages/(admin)/products/ProductsAdd";
 import ProductsDetail from "./pages/(admin)/products/ProductsDetail";
+import CategoryList from "./pages/(admin)/categorys/CategoryList";
+import CategoryAdd from "./pages/(admin)/categorys/CategoryAdd";
+import CategoryUpdate from "./pages/(admin)/categorys/CategoryUpdate";
 function App() {
   const configRouter = createBrowserRouter([
     {
@@ -16,6 +19,18 @@ function App() {
           element: <DashBoard />,
         },
         {
+          path: "/categorys",
+          element: <CategoryList />,
+        },
+        {
+          path: "/categorys/addcategory",
+          element: <CategoryAdd />,
+        },
+        {
+          path: "/categorys/update/:id",
+          element: <CategoryUpdate />,
+        },
+        {
           path: "/products",
           element: <ProductsList />,
         },
@@ -23,18 +38,15 @@ function App() {
           path: "/products/edit/:id",
           element: <ProductsEdit />,
         },
-        
+
         {
           path: "/products/add",
           element: <ProductsAdd />,
-        }
-        ,
+        },
         {
           path: "/products/detail/:id",
           element: <ProductsDetail />,
-        }
-        
-    
+        },
       ],
     },
   ]);
