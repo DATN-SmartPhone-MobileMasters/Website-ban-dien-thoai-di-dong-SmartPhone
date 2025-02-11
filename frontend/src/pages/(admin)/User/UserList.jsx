4953d83509d0af2d1d58d03ed252e31e6a0a1b16
Anhnpdph47088 <162577181+Anhnpdph47088@users.noreply.github.com>
2025-02-11 07:10:44 +0700

    Update UserList.jsx

--- frontend/src/pages/(admin)/User/UserList.jsx
+++ frontend/src/pages/(admin)/User/UserList.jsx
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'; // npm i axios
+import axios from 'axios';// npm i axios
 import { useNavigate } from 'react-router-dom';
-import { confirmAlert } from 'react-confirm-alert'; // npm i react-confirm-alert
-import 'react-confirm-alert/src/react-confirm-alert.css';
-import '../../../App.css'; // Import App.css
+import { confirmAlert } from "react-confirm-alert"; //npm i react-confirm-alert
+import "react-confirm-alert/src/react-confirm-alert.css";
+
 const UserList = () => {
   const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -18,13 +16,11 @@ const UserList = () => {
     try {
       setIsLoading(true);
       const res = await axios.get('http://localhost:5000/api/users');
-      setUsers(res.data);
+      setUsers(res.data.data);
     } catch (e) {
       console.error(e);
-      setError('Không tìm thấy dữ liệu, mời thử lại');
-    } finally {
-      setIsLoading(false);
-    }
+      
+    } 
   };
 
   const handleDelete = async (id) => {
@@ -40,7 +36,6 @@ const UserList = () => {
               setUsers(users.filter(user => user._id !== id));
             } catch (e) {
               console.error(e);
-              setError('Lỗi, mời thử lại.');
             }
           },
         },
@@ -58,12 +53,8 @@ const UserList = () => {
     navigate(`/accounts/${id}`);
   };
 
-  if (isLoading) {
+  if (!user) {
     return <div className="text-center mt-5">Loading...</div>;
-  }
-
-  if (error) {
-    return <div className="text-center mt-5 text-red-500">{error}</div>;
   }
 
   return (
@@ -99,13 +90,13 @@ const UserList = () => {
                     <td className="space-x-2">
                       <button 
                         onClick={() => handleViewDetails(user._id)}
-                        className="btn btn-warning ml-2"
+                        className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-1 px-3 rounded text-xs transition duration-200 ease-in-out shadow-md hover:shadow-lg mr-2"
                       >
                         Chi Tiết 
                       </button>
                       <button 
                         onClick={() => handleDelete(user._id)}
-                        className="btn btn-danger ml-2"
+                        className="bg-red-500 hover:bg-red-600 text-white font-semibold py-1 px-3 rounded text-xs transition duration-200 ease-in-out shadow-md hover:shadow-lg"
                       >
                         Xoá
                       </button>
