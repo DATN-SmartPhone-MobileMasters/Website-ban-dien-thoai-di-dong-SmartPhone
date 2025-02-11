ecc386236c44b4b3a8243ae2ef3ee31035e4bdd2
Anhnpdph47088 <162577181+Anhnpdph47088@users.noreply.github.com>
2025-02-11 20:05:07 +0700

    tách api Users

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -6,5 +6,9 @@ export const createBrand = async (data) => axios.post(`${API_URL}/thuonghieus`, 
 export const updateBrand = async (id, data) => axios.put(`${API_URL}/thuonghieus/${id}`, data);
 export const deleteBrand = async (id) => axios.delete(`${API_URL}/thuonghieus/${id}`);
 export const getBrandById = async (id) => axios.get(`${API_URL}/thuonghieus/${id}`);
+//users
+export const fetchUsers = async () => axios.get(`${API_URL}/users`);
+export const deleteUser = async (id) => axios.delete(`${API_URL}/users/${id}`);
+export const getUserById = async (id) => axios.get(`${API_URL}/users/${id}`);
 
 export const fetchCategories = async () => axios.get(`${API_URL}/danhmucs`);
