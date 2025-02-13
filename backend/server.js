d122e295cd587f4edb25a7d9699e537815bdb24d
tuannguyen-1309 <tuandungyn2@gmail.com>
2025-02-13 20:08:10 +0700

    server be

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,7 @@ app.set("views", "./views");
 mongoose.connect(process.env.MONGO_URI).then((result) => {
   // Router
   app.use("/", router);
+  app.use(cors());
   // Start server
   app.listen(port, () => {
     console.log(`Server đang chạy ở port ${port}`);
