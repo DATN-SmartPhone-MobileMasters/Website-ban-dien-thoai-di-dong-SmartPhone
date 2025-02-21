c542ea2ef0446a1363a3c977cfd20f3a6fa208f7
Anhnpdph47088 <162577181+Anhnpdph47088@users.noreply.github.com>
2025-02-21 14:59:15 +0700

    Update signup-form.jsx

--- frontend/src/pages/(website)/Users/signup-form.jsx
+++ frontend/src/pages/(website)/Users/signup-form.jsx
@@ -134,7 +134,7 @@ const SignupForm = () => {
         <div className="w-1/2 p-8">
           <div className="mb-8">
             <div className="flex items-center">
-              <img src="./src/./img/feature_icon_1.png" className="w-12 mr-4" />
+              <img src="/src/./img/feature_icon_1.png" className="w-12 mr-4" />
               <div>
                 <h4 className="text-lg font-bold text-black">Mức độ uy tín!</h4>
                 <p className="text-sm text-gray-600">Được đánh giá an toàn, tin cậy hàng đầu Việt Nam với nhiều chính sách hỗ trợ chăm sóc khách hàng.</p>
@@ -143,7 +143,7 @@ const SignupForm = () => {
           </div>
           <div className="mb-8">
             <div className="flex items-center">
-              <img src="./src/./img/feature_icon_2.png" className="w-12 mr-4" />
+              <img src="/src/./img/feature_icon_2.png" className="w-12 mr-4" />
               <div>
                 <h4 className="text-lg font-bold text-black">Thanh toán tức thì!</h4>
                 <p className="text-sm text-gray-600">Thanh toán mọi nơi mọi lúc, giao dịch nhanh gọn, bảo đảm, an toàn, với liên kết 90% ngân hàng, ví tiền, VISA trong toàn quốc!</p>
@@ -152,7 +152,7 @@ const SignupForm = () => {
           </div>
           <div className="mb-8">
             <div className="flex items-center">
-              <img src="./src/./img/feature_icon_3.png" className="w-12 mr-4" />
+              <img src="/src/./img/feature_icon_3.png" className="w-12 mr-4" />
               <div>
                 <h4 className="text-lg font-bold text-black">Ưu đãi hấp dẫn!</h4>
                 <p className="text-sm text-gray-600">Với mong muốn làm hài lòng khách hàng, Mobistore luôn mang đến những ưu đãi cực kỳ tốt với chất lượng cao</p>
