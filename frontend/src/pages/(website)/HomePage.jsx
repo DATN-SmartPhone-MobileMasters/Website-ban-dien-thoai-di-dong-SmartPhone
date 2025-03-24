ec2f936e55d72c8ce26a20a8b4a1b5bf8c6f02b6
tuannguyen-1309 <tuandungyn2@gmail.com>
2025-03-24 22:42:53 +0700

    xóa thương hiệu

--- frontend/src/pages/(website)/HomePage.jsx
+++ frontend/src/pages/(website)/HomePage.jsx
@@ -3,16 +3,10 @@ import LatestProducts from "./components/LatestProducts";
 import SellerProducts from "./components/SellerProducts";
 import Features from "./components/Features";
 import Slider from "./components/Slider";
-import MobileShowcase from "./components/MobileShowCase";
 
 const HomePage = () => {
   return (
     <div className="flex flex-col md:flex-row min-h-screen bg-gray-50 px-10">
-      {/* Sidebar */}
-      <aside className="w-full md:w-[250px] p-4 flex justify-center md:justify-start">
-        <MobileShowcase />
-      </aside>
-      
       {/* Main Content */}
       <main className="flex-1 p-6 space-y-6">
         {/* Slider */}
