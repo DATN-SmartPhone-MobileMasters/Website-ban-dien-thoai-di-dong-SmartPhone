246d47e883d93b67314d5cbbd00a845930b10eec
khanhdao901 <khanhddph47074@fpt.edu.vn>
2025-03-31 16:58:17 +0700

    thống kê bình luận

--- frontend/src/pages/(admin)/statistical/ThongKe.jsx
+++ frontend/src/pages/(admin)/statistical/ThongKe.jsx
@@ -99,7 +99,7 @@ const ThongKe = () => {
         <div className="col-xl-4 col-md-6 mb-4">
           <div className="card border-left-primary shadow h-100 py-2">
             <div className="card-body">
-              <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Tuần:</div>
+              <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Tổng:</div>
               <div className="h5 mb-0 font-weight-bold text-gray-800">
                 <p>{thongKeDoanhThu.tongDoanhThuTheoTuan.toLocaleString("vi-VN", { style: "currency", currency: "VND" })}</p>
               </div>
